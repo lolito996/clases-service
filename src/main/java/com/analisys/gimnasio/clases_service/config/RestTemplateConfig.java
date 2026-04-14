c9ec53fd941e093d0286f682bf1f473be0545619
lolito996 <josealejandromc4@gmail.com>
2026-04-13 19:51:17 -0500

    feat: add Spring Eureka and LoadBalancer

--- src/main/java/com/analisys/gimnasio/clases_service/config/RestTemplateConfig.java
+++ src/main/java/com/analisys/gimnasio/clases_service/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.analisys.gimnasio.clases_service.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getInterceptors().add(bearerTokenRelayInterceptor());
